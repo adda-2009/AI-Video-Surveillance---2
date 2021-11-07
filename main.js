09526c5c6832c942d2c197e2eb2ad2fdeed0c61e
adda-2009 <78640709+adda-2009@users.noreply.github.com>
2021-11-07 10:42:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+status="";
+video="";
+
+function preload()
+{
+video=createVideo('video.mp4');
+}
+
+function setup()
+{
+canvas=createCanvas(480,380);
+canvas.center();
+video.hide();
+}
+
+function draw()
+{
+image(video,0,0,480,380);
+}
+
+function start()
+{
+objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById('status').innerHTML="status:detecting objects"
+}
+
+function modelLoaded()
+{
+console.log("modelLoaded");
+status=true;
+video.loop();
+video.speed(1);
+video.volume(0);
+}
